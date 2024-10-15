9919da664aeba82930fe92c5d8ff7d4b9293897a
DenysFedorenkoA <121401812+DenysFedorenkoA@users.noreply.github.com>
2024-10-15 20:57:32 +0300

    Delete New project (24) directory

--- New project (24)/scripts/c3main.js
+++
@@ -1,4 +0,0 @@
-import "./c3runtime.js";
-import "./plugins/LostTelegramMiniAppsSDK/c3runtime/main.js";
-import "./plugins/LostTelegramPaymentsAPI/c3runtime/main.js";
-import "./objRefTable.js";
--- New project (24)/scripts/plugins/TelegramMiniAppsSDK/c3runtime/plugin.js
+++
@@ -1,7 +0,0 @@
-const C3 = globalThis.C3;
-C3.Plugins.TelegramMiniAppsSDK = class SingleGlobalPlugin extends globalThis.ISDKPluginBase {
-    constructor() {
-        super();
-    }
-};
-// export {};
