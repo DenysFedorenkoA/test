8425a235a9ac59df55b0492e2842e222c0c532a0
DenysFedorenkoA <121401812+DenysFedorenkoA@users.noreply.github.com>
2024-10-15 06:08:37 -0700

    Add files via upload

---
+++ New project (24)/scripts/c3main.js
@@ -0,0 +1,3 @@
+import "./c3runtime.js";
+import "./plugins/TelegramMiniAppsSDK/c3runtime/main.js";
+import "./objRefTable.js";
---
+++ New project (24)/scripts/objRefTable.js
@@ -0,0 +1,39 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.TelegramMiniAppsSDK,
+		C3.Plugins.Text,
+		C3.Plugins.Touch,
+		C3.Plugins.Dictionary,
+		C3.Plugins.System.Cnds.OnLayoutStart,
+		C3.Plugins.Text.Acts.SetText,
+		C3.Plugins.TelegramMiniAppsSDK.Exps.UserId,
+		C3.Plugins.TelegramMiniAppsSDK.Exps.UserFirstName,
+		C3.Plugins.TelegramMiniAppsSDK.Exps.CloudStorageValue,
+		C3.Plugins.Dictionary.Acts.JSONLoad,
+		C3.Plugins.System.Cnds.IsValueType,
+		C3.Plugins.System.Acts.SetVar,
+		C3.Plugins.Text.Cnds.HasTags,
+		C3.Plugins.Dictionary.Exps.AsJSON,
+		C3.Plugins.Touch.Cnds.OnTapGesture,
+		C3.Plugins.System.Acts.AddVar,
+		C3.Plugins.Dictionary.Acts.AddKey,
+		C3.Plugins.TelegramMiniAppsSDK.Acts.CloudStorageSetItem
+	];
+};
+self.C3_JsPropNameTable = [
+	{TelegramMiniAppsSDK: 0},
+	{Text: 0},
+	{Touch: 0},
+	{Dictionary: 0},
+	{Text2: 0},
+	{score: 0}
+];
+
+self.InstanceType = {
+	TelegramMiniAppsSDK: class extends self.C3.Plugins.TelegramMiniAppsSDK.Instance {},
+	Text: class extends self.ITextInstance {},
+	Touch: class extends self.IInstance {},
+	Dictionary: class extends self.IDictionaryInstance {},
+	Text2: class extends self.ITextInstance {}
+}
