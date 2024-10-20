0eda2025282451ed2d3a0f8f660cb8ab16b300cd
DenysFedorenkoA <121401812+DenysFedorenkoA@users.noreply.github.com>
2024-10-20 12:38:07 +0300

    Delete New project (36) directory

--- New project (36)/scripts/c3main.js
+++
@@ -1,5 +0,0 @@
-import "./c3runtime.js";
-import "./plugins/LostTelegramPaymentsAPI/c3runtime/main.js";
-import "./objRefTable.js";
-import "./project/telegram-web-app.js";
-import "./project/scriptsInEvents.js";
