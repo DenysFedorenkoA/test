250c36ad3fc7076a01b8dad394705b46bf9302d9
DenysFedorenkoA <121401812+DenysFedorenkoA@users.noreply.github.com>
2024-12-06 17:33:32 +0200

    Delete New project (11) directory

--- New project (11)/scripts/plugins/LostTelegramMiniAppsSDK/c3runtime/type.js
+++
@@ -1,9 +0,0 @@
-const Config = {AddonId: "LostTelegramMiniAppsSDK"};
-const C3 = globalThis.C3;
-C3.Plugins[Config.AddonId].Type = class LostType extends globalThis.ISDKObjectTypeBase {
-    constructor() {
-        super();
-    }
-    _onCreate() { }
-};
-export {};
