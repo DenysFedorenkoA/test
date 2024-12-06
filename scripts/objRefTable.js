308f56b40e4f116a4f689a20e1d81c5b5b44b83d
DenysFedorenkoA <121401812+DenysFedorenkoA@users.noreply.github.com>
2024-12-06 17:45:40 +0200

    Add files via upload

--- scripts/objRefTable.js
+++ scripts/objRefTable.js
@@ -3,16 +3,21 @@ self.C3_GetObjectRefTable = function () {
 	return [
 		C3.Plugins.LostTelegramMiniAppsSDK,
 		C3.Plugins.Text,
+		C3.Plugins.Touch,
 		C3.Plugins.LostTelegramMiniAppsSDK.Cnds.onCloudStorageLoaded,
-		C3.Plugins.Text.Acts.SetText
+		C3.Plugins.Text.Acts.SetText,
+		C3.Plugins.Touch.Cnds.OnTouchObject,
+		C3.Plugins.LostTelegramMiniAppsSDK.Acts.setItem
 	];
 };
 self.C3_JsPropNameTable = [
 	{TelegramMiniApps: 0},
-	{Text: 0}
+	{Text: 0},
+	{Touch: 0}
 ];
 
 self.InstanceType = {
 	TelegramMiniApps: class extends self.C3.Plugins.LostTelegramMiniAppsSDK.Instance {},
-	Text: class extends self.ITextInstance {}
+	Text: class extends self.ITextInstance {},
+	Touch: class extends self.IInstance {}
 }
